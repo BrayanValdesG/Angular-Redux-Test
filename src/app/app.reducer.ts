fca6f5c4cb3ac734b60315cd7ef07b1d4f5fafe8
BrayanValdesG <balexis41@gmail.com>
2021-07-02 18:47:19 -0500

    unnecesary store - ui

--- src/app/app.reducer.ts
+++ src/app/app.reducer.ts
@@ -1,14 +1,10 @@
 import { environment } from '@environments/environment';
 import { ActionReducerMap, MetaReducer } from '@ngrx/store';
 
-import * as ui from '@shared/store/ui.reducer';
-
 export interface AppState {
-    ui: ui.State
 }
 
 export const appReducers: ActionReducerMap<AppState> = {
-    ui: ui.uiReducer
 }
 
 export const metaReducers: MetaReducer<AppState>[] = !environment.production ? [] : [];
--- src/app/shared/store/ui.actions.ts
+++
@@ -1,4 +0,0 @@
-import { createAction } from '@ngrx/store';
-
-export const isLoading = createAction('[UI Loading] isLoading');
-export const stopLoading = createAction('[UI Loading] stopLoading');
